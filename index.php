1deab6c5b5f76ae005a98723058ec2ba8752ab5d
Juscelino Junior <juniorjuscelino122@gmail.com>
2019-12-08 12:55:46 -0300

    iniciando a batalha, ainda sem o resultado

--- index.php
+++ index.php
@@ -6,8 +6,24 @@
     <link rel="stylesheet" href="index.css">
   </head>
   <body>
-    <?php 
-        include('classes.php'); 
-    ?>
+    <?php include('classes.php'); ?>
+    <div>
+        <h1>BATALHA</h1>
+        <form action = " " method = "post">
+            <select name = "carta1">
+                <?php foreach ($cartas as $value): ?>
+                    <?php echo "<option value=",$value->nome,">",$value->nome,"</option>"; ?>
+                <?php endforeach; ?>
+            </select>
+        </form>
+        "VS"
+        <form action = " " method = "post">
+            <select name = "carta2">
+                <?php foreach ($cartas as $value): ?>
+                    <?php echo "<option value=",$value->nome,">",$value->nome,"</option>"; ?>
+                <?php endforeach; ?>
+            </select>
+        </form>
+    </div> 
   </body>
 </html>
